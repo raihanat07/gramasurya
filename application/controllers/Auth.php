16553b45fb951fa931d6ee8f5181973de6ca392f
wendry12 <48092764+wendry12@users.noreply.github.com>
2021-04-01 23:26:56 +0700

    buat auth untuk login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,10 +7,30 @@ class Auth extends CI_Controller {
 	{
 		$this->load->view('login');
 	}
-
+  
     public function process()
     {
         $post = $this->input->post(null, TRUE);
-        if(isset($post['login']))
+        if(isset($post['login'])){
+            $this->load->model('user_m');
+            $query = $this->user_m->login($post);
+            if($query->num_rows() > 0){
+                $row = $query->row();
+                $params = array(
+                    'id_user' => $row->id_user,
+                    'level' =>$row->level
+                );
+                $this->session->set_userdata($params);
+                echo "<script>
+                    alert('Selamat Login Berhasil')
+                    window.location = '".site_url('dashboard')."';
+                </script>";
+            }else {
+                echo "<script>
+                    alert('Astaga Login Gagal')
+                    window.location = '".site_url('auth/login')."';
+                </script>";
+            }
+        }
     }
 }
