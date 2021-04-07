16829d723a1bac50ceb1d619185be24a7014ac8f
wendry12 <48092764+wendry12@users.noreply.github.com>
2021-04-08 00:14:16 +0700

    form validation tambah user

--- application/controllers/pracetak/User.php
+++ application/controllers/pracetak/User.php
@@ -13,8 +13,27 @@ class User extends CI_Controller {
 	
 	public function tambah_user()
 	{
-		$data['judul'] = 'Tambah User';
-		$this->template->load('pracetak/template','pracetak/user/tambah_user', $data );
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('level', 'Level', 'required');
+		$this->form_validation->set_rules('username', 'Username', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('passconf', 'Konfirmasi Password', 'required|matches[password]',
+			array('matches' => '%s tidak sesuai dengan password')
+		);
+		$this->form_validation->set_message('required', '%s masih kosong');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$data['judul'] = 'Tambah User';
+			$this->template->load('pracetak/template','pracetak/user/tambah_user', $data );
+		}
+		else
+		{
+				echo "berhasil";
+		}
+		
 	}
 	
 }
