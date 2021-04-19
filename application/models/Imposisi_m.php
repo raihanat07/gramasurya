675cd06dcf22d4b99ddeee3e8d99d3661cec8302
raihanat07 <raihanaqila70@gmail.com>
2021-04-19 23:49:49 +0700

    klem

--- application/models/Imposisi_m.php
+++ application/models/Imposisi_m.php
@@ -7,7 +7,7 @@ class Imposisi_m extends CI_Model {
     {
         $this->db->select(
             'order.id_order as id_order, order.tanggal_masuk as tanggal_masuk, order.deadline as deadline, order.nama_pemesan as nama_pemesan,  order.nama_orderan as nama_orderan, order.ukuran as ukuran, order.halaman as halaman, order.oplag as oplag, order.so_status as so_status, 
-            finishing.finishing_akhir_bending as bending, finishing.finishing_akhir_hard_cover as hard_cover, finishing.finishing_akhir_jahit_benang as jahit_benang, finishing.finishing_akhir_jahit_kawat as jahit_kawat, finishing.finishing_akhir_pond as pond, finishing.finishing_akhir_spiral as spiral',
+            finishing.finishing_akhir_bending as bending, finishing.finishing_akhir_hard_cover as hard_cover, finishing.finishing_akhir_jahit_benang as jahit_benang, finishing.finishing_akhir_jahit_kawat as jahit_kawat, finishing.finishing_akhir_pond as pond,finishing.finishing_akhir_klem as klem, finishing.finishing_akhir_spiral as spiral',
         );
         $this->db->from('order');
         $this->db->join('finishing','finishing.id_order = order.id_order');
