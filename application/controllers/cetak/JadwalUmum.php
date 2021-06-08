a6c52e2d9a8adf7411d512c745c76ec1d2cafcd8
wendry12 <48092764+wendry12@users.noreply.github.com>
2021-06-09 04:46:44 +0700

    ju tambah ( belum selesai )

--- application/controllers/cetak/JadwalUmum.php
+++ application/controllers/cetak/JadwalUmum.php
@@ -3,10 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class JadwalUmum extends CI_Controller {
 
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('JadwalUmum_m', 'ju');
+    }
+
 	public function index()
 	{
-		// check_already_login_cetak();
-		$data['judul'] = 'Jadwal Umum Cetak';
+		$query = $this->ju->get();
+		$data = array(
+			'judul' => 'Jadwal Umum',
+			'ju' => $query->result(),
+		);
 		$this->template->load('cetak/template','cetak/jadwal_umum/jadwalumum',$data);
 	}
 	public function lihat_ju()
@@ -20,6 +29,65 @@ class JadwalUmum extends CI_Controller {
 		// check_already_login_cetak();
 		$data['judul'] = 'Jadwal Umum Cetak';
 		$this->template->load('cetak/template','cetak/jadwal_umum/jadwalumum-edit',$data);
+	}
+
+	public function proses()
+	{		
+		if(isset($_POST['add'])){							
+			$inputan = $this->input->post(null, TRUE);
+			
+			
+			// if($inputan["jumlah_kertas_cover1"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_cover1"];
+			// }
+			// if($inputan["jumlah_kertas_cover2"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_cover2"];
+			// }
+
+			// if($inputan["jumlah_kertas_isi1"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi1"];
+			// }
+			// if($inputan["jumlah_kertas_isi2"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi2"];
+			// }
+			// if($inputan["jumlah_kertas_isi3"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi3"];
+			// }
+
+			// if($inputan["target_cover"] !=null){
+			// 	$inputan["druk_cover"] = $inputan["target_cover"] + ["oplag"];
+			// }
+
+			$this->ju->tambah_ju($inputan);							
+			// $this->ctcp->status_umum($inputan);						
+				echo "<script> alert('Data Berhasil Ditambahkan'); </script>";		
+				echo "<script>window.location='".site_url('cetak/jadwalumum')."'; </script>";
+
+		} else if(isset($_POST['edit'])){ 
+			$inputan = $this->input->post(null, TRUE);
+
+			// if($inputan["jumlah_kertas_cover1"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_cover1"];
+			// }
+			// if($inputan["jumlah_kertas_cover2"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_cover2"];
+			// }
+
+			// if($inputan["jumlah_kertas_isi1"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi1"];
+			// }
+			// if($inputan["jumlah_kertas_isi2"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi2"];
+			// }
+			// if($inputan["jumlah_kertas_isi3"] !=null){
+			// 	$inputan["total_kertas"] = $inputan["total_kertas"] + $inputan["jumlah_kertas_isi3"];
+			// }
+
+			$this->dc->tambah_dc($inputan);							
+			// $this->dc->status_umum($inputan);						
+				echo "<script> alert('Data Berhasil Diubah'); </script>";		
+				echo "<script>window.location='".site_url('cetak/displaycetak')."'; </script>";
+		}	
 	}
 
 }
