d3c6ef1f9a8af95c1310e3e3c1ee0c0a454cab37
wendry12 <48092764+wendry12@users.noreply.github.com>
2021-07-16 19:09:24 +0700

    insit

--- application/views/cetak/jadwal_mesin/mesin_72/mesin72-tambah-spk.php
+++ application/views/cetak/jadwal_mesin/mesin_72/mesin72-tambah-spk.php
@@ -559,7 +559,15 @@
                   Insit
                 </div>
                 <div class="col-md-4">
-                  <label>dari db</label>
+                  <label><?php if($row->jenis_cetakan == "Cover"){ ?>
+                        <?php echo $row->insit_cover_1!=null && $row->insit_cover_1!='-'? "".$row->insit_cover_1."<br>": "" ?>
+                        <?php echo $row->insit_cover_2!=null && $row->insit_cover_2!='-'? "".$row->insit_cover_2."<br>": "" ?>
+                        <?php echo $row->insit_cover_3!=null && $row->insit_cover_3!='-'? "".$row->insit_cover_3."<br>": "" ?>
+                      <?php } else if ($row->jenis_cetakan == "Isi"){?>
+                        <?php echo $row->insit_isi_1!=null && $row->insit_isi_1!='-'? "".$row->insit_isi_1."<br>": "" ?>
+                        <?php echo $row->insit_isi_2!=null && $row->insit_isi_2!='-'? "".$row->insit_isi_2."<br>": "" ?>
+                        <?php echo $row->insit_isi_3!=null && $row->insit_isi_3!='-'? "".$row->insit_isi_3."<br>": "" ?>
+                      <?php } ?></label>
                 </div>
               </div><br>
               <div class="row">
